46ff4c4660447f3fb9597e8b727c9eca49f6c753
R <50966008+rashmika13@users.noreply.github.com>
2020-08-28 15:02:10 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>KANBAN</h1>
+    </div>
+  );
+}
+
+export default App;
